c4027293385f39716bef7c196fec01c03149c2c9
Alberto Licea <albertolicea00@icloud.com>
2023-07-01 16:54:38 -0400

    acondicionando componente a vista

--- app/Http/Livewire/Auth/ForgotPassword.php
+++ app/Http/Livewire/Auth/ForgotPassword.php
@@ -12,7 +12,7 @@ class ForgotPassword extends Component
     use Notifiable;
 
     public $email='';
-    
+
     protected $rules = [
         'email' => 'required|email',
     ];
@@ -38,19 +38,19 @@ class ForgotPassword extends Component
 
         $user = User::where('email', $this->email)->first();
 
-    
+
         if($user){
 
-    
-            $this->notify(new ResetPassword($user->id));
+            // $this->notify(new ResetPassword($user->id));
+            // return back()->with('status', "¡Hemos enviado por correo electrónico el enlace para restablecer tu contraseña!");
 
-            return back()->with('status', "We have emailed your password reset link!");
+            return back()->with('in_process', "Esta función aun esta en construcción");
 
-    
+
         } else {
-    
-            return back()->with('email', "We can't find any user with that email address.");
-    
+
+            return back()->with('email', "No podemos encontrar ningún usuario con esa dirección de correo electrónico.");
+
         }
     }
 }
--- app/Http/Livewire/Auth/Register.php
+++ app/Http/Livewire/Auth/Register.php
@@ -10,24 +10,31 @@ class Register extends Component
 
     public $name ='';
     public $email = '';
+    public $phone = '';
+    public $message = '';
     public $password = '';
 
     protected $rules=[
-    'name' => 'required|min:3',
-    'email' => 'required|email|unique:users,email',
-    'password' => 'required|min:5',];
+        'name' => 'required|min:3',
+        'email' => 'required|email|unique:users,email',
+        'phone' => 'required',
+        'message' => 'required|min:3|max:250',
+        'password' => 'required|min:5',
+    ];
 
 
     public function store(){
 
-        $attributes = $this->validate();
+        // $attributes = $this->validate();
 
-        $user = User::create($attributes);
+        // $user = User::create($attributes);
 
-        auth()->login($user);
-        
-        return redirect('/dashboard');
-    } 
+        // auth()->login($user);
+        // return redirect('/dashboard');
+        //return back()->with('status', "En breve se le enviará un email con la respuesta a su solicitud");
+
+        return back()->with('in_process', "Esta función aun esta en construcción");
+    }
 
     public function render()
     {
