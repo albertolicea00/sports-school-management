d2677a2b69a0a796effad1ebf7cf47eadc19a35a
Alberto Licea <albertolicea00@icloud.com>
2023-09-20 11:09:11 -0400

    migraciones de primera metrica (test de performance a atletas)

---
+++ database/migrations/2025_09_20_143648_create_athetes_made_test_performance_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('athetes_made_test_performance', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('athlete_id');
+            $table->unsignedBigInteger('coach_id');
+            $table->float('made_at')->nullable();
+            $table->json('tests');
+            $table->string('about')->nullable();
+            $table->boolean('enable')->default(1);
+            $table->json('meta')->nullable();
+            $table->timestamps();
+
+            $table->foreign('athlete_id')->references('id')->on('athletes')->constrained();
+            // ->onUpdate('cascade')
+            // ->onDelete('cascade');
+            $table->foreign('coach_id')->references('id')->on('coaches')->constrained();
+            // ->onUpdate('cascade')
+            // ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('athetes_made_test_performance');
+    }
+};
