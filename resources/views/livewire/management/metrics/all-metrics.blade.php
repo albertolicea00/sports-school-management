866ff33efedca1c379b4885b49d29c8cf3d5e50e
Alberto Licea <albertolicea00@icloud.com>
2023-10-06 21:24:16 -0400

    visual listado de metricas

--- resources/views/livewire/management/metrics/all-metrics.blade.php
+++ resources/views/livewire/management/metrics/all-metrics.blade.php
@@ -1,44 +1,33 @@
 <div>
-    <div class="row">
-        <div class="col-md-8 mt-4">
-            <div class="card">
-                <div class="card-header pb-0 px-3">
-                    <h6 class="mb-0">Tests</h6>
-                </div>
-                <div class="card-body pt-4 p-3">
-                    <ul class="row">
-                        <li class="col list-group-item border-0 d-flex p-4 m-1 bg-gray-100 border-radius-lg">
-                            <div class="d-flex flex-column">
-                                <h6 class="mb-3 text-sm">Oliver Liam</h6>
-                                <span class="mb-2 text-xs">Company Name: <span
-                                        class="text-dark font-weight-bold ms-sm-2">Viking
-                                        Burrito</span></span>
-                                <span class="mb-2 text-xs">Email Address: <span
-                                        class="text-dark ms-sm-2 font-weight-bold">oliver@burrito.com</span></span>
-                                <span class="text-xs">VAT Number: <span
-                                        class="text-dark ms-sm-2 font-weight-bold">FRB1235476</span></span>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+    <div class="d-flex justify-content-between mx-5 my-3">
+        <h6 class="h2">Todas las Metricas</h6>
+        {{-- ACCIONES --}}
+        <div>
+            <div class="btn btn-outline-primary mt-2">FILTRAR</div>
+            {{-- <div class="btn btn-outline-primary mt-2">BUSCAR</div>
+            <div class="btn btn-primary mt-2">NUEVO TEST</div>
+            <div class="btn btn-primary mt-2">NUEVO EVALUACIÓN</div> --}}
         </div>
-        <div class="col-md-4 mt-4">
-            <div class="card h-100 mb-4">
-                <div class="card-header pb-0 px-3">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <h6 class="mb-0">Todas las Metricas</h6>
+    </div>
+
+
+    @forelse ($metric_categories as $category)
+        <div class="m-4">
+            <div class="row">
+                <div class="col">
+                    <div class="card h-100 mb-4">
+                        <div class="card-header pb-0 px-3 pt-2">
+                            <h6 class="text-uppercase text-body text-md font-weight-bolder "><i class="{{ $category->icon }}"></i> {{ $category->name }}</h6>
                         </div>
-                        <div class="col-md-6 d-flex justify-content-start justify-content-md-end align-items-center">
+                        <div class="card-body pt-2 p-3">
+                            @include('livewire\management\metrics\layout\metric-list', ['category' => $category])
                         </div>
                     </div>
                 </div>
-                <div class="card-body pt-2 p-3">
-                    @include('livewire\management\metrics\layout\metric-list')
-                </div>
             </div>
         </div>
-    </div>
-</div>
+    @empty
+        No hay nada
+    @endforelse
+
 </div>
--- resources/views/livewire/management/metrics/layout/metric-list.blade.php
+++ resources/views/livewire/management/metrics/layout/metric-list.blade.php
@@ -1,37 +1,31 @@
-@forelse ($metric_categories as $category)
-<h6 class="text-uppercase text-body text-xs font-weight-bolder mt-3"><i
-        class="{{ $category->icon }}"></i> {{ $category->name }}</h6>
+<div class="row">
+    @forelse ($category->metric_models as $metric)
+    <div class="col-xs-6 col-sm-5 col-md-4 col-lg-4 col-xl-3">
+        <div class="d-flex align-items-center py-4 px-3" style="background-color: ghostwhite;border-radius: 20px;">
+            <a href="metric/{{ $category->route . $metric->route }}"
+                class="btn btn-icon-only btn-rounded btn-outline-danger mb-0 me-3 p-3 btn-sm d-flex align-items-center justify-content-center"
+                style="border-color: {{ $metric->color }};color:{{ $metric->color }}">
+                <i class="{{ $metric->icon }}" style="transform: scale(1.9);"></i>
+            </a>
 
-@forelse ($category->metric_models as $metric)
-<li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-    <div class="d-flex align-items-center">
-        <a href="metric/{{ $category->route . $metric->route }}"
-            class="btn btn-icon-only btn-rounded btn-outline-danger mb-0 me-3 p-3 btn-sm d-flex align-items-center justify-content-center"
-            style="border-color: {{ $metric->color }};color:{{ $metric->color }}">
-            <i class="{{ $metric->icon }}" style="transform: scale(1.9);"></i>
-        </a>
-
-        <div class="d-flex flex-column">
-            <h6 class="mb-1 text-dark text-sm">{{ $metric->name_p }}</h6>
-            <span class="text-xs">
-                {{ $metric->sport ? $metric->sport->name : ''}}
-                @if (count($metric->school_grade) != 0 && $metric->sport)
-                &nbsp; || &nbsp;
-                @endif
-                {{ count($metric->school_grade) != 0 ? implode(', ',
-                $metric->school_grade->pluck('name')->toArray()) . ' años' : '' }}
-            </span>
+            <div class="d-flex flex-column">
+                <h6 class="mb-1 text-dark text-md">{{ $metric->name_p }}</h6>
+                <span class="text-xs">
+                    {{ $metric->sport ? $metric->sport->name : ''}}
+                    @if (count($metric->school_grade) != 0 && $metric->sport)
+                    &nbsp; || &nbsp;
+                    @endif
+                    {{ count($metric->school_grade) != 0 ? implode(', ',
+                    $metric->school_grade->pluck('name')->toArray()) . ' años' : '' }}
+                </span>
+            </div>
         </div>
-    </div>
-    <div class="d-flex align-items-center text-danger text-gradient text-sm font-weight-bold">
+        {{-- <div class="d-flex align-items-center text-danger text-gradient text-sm font-weight-bold">
 
+        </div> --}}
     </div>
-</li>
-@empty
-No hay nada
-@endforelse
+    @empty
+    No hay nada
+    @endforelse
+</div>
 
-</ul>
-@empty
-No hay nada
-@endforelse
