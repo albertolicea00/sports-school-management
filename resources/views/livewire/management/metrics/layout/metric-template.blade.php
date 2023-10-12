1643f2f2c2e8d112ac119b286520d76c312eb030
Alberto Licea <albertolicea00@icloud.com>
2023-10-12 07:29:35 -0400

    fix refresh when sucess some

--- resources/views/livewire/management/metrics/layout/metric-template.blade.php
+++ resources/views/livewire/management/metrics/layout/metric-template.blade.php
@@ -1,57 +1,60 @@
-<div class="d-flex justify-content-between mx-5 my-3">
-    <div class="d-flex flex-column justify-content-center">
-        <h6 class="h2 mb-0">
-            <i class="{{ $icon }}" style="transform: scale(.8);"></i>
-            {{ $title }}
-        </h6>
-        <small class="h5 px-1" style="font-weight: initial;">
-            {{ $sport }}
-        </small>
+<div>
+    <div class="d-flex justify-content-between mx-5 my-3">
+        <div class="d-flex flex-column justify-content-center">
+            <h6 class="h2 mb-0">
+                <i class="{{ $icon }}" style="transform: scale(.8);"></i>
+                {{ $title }}
+            </h6>
+            <small class="h5 px-1" style="font-weight: initial;">
+                {{ $sport }}
+            </small>
+        </div>
+
+        {{-- ACCIONES --}}
+        <div>
+            @yield('metric-template-actions')
+            {{-- <div class="btn btn-outline-primary mt-2">NORMA</div> --}}
+            {{-- <div class="btn btn-outline-primary mt-2">BUSCAR</div> --}}
+            {{-- <div class="btn btn-outline-primary mt-2">NORMA</div> --}}
+            @if ($is_coache_test) <div class="btn btn-primary mt-2">EVALUAR ATLETA</div> @endif
+            @if ($is_athlete_test) <div class="btn btn-primary mt-2">EVALUAR ENTRENADOR</div> @endif
+        </div>
     </div>
 
-    {{-- ACCIONES --}}
-    <div>
-        @yield('metric-template-actions')
-        {{-- <div class="btn btn-outline-primary mt-2">NORMA</div> --}}
-        {{-- <div class="btn btn-outline-primary mt-2">BUSCAR</div> --}}
-        {{-- <div class="btn btn-outline-primary mt-2">NORMA</div> --}}
-        @if ($is_coache_test) <div class="btn btn-primary mt-2">EVALUAR ATLETA</div> @endif
-        @if ($is_athlete_test) <div class="btn btn-primary mt-2">EVALUAR ENTRENADOR</div> @endif
-    </div>
-</div>
+    @yield('metric-template-content')
+    @yield('metric-template-info')
+    @yield('metric-template-specific-section')
 
-@yield('metric-template-info')
-@yield('metric-template-content')
-@yield('metric-template-specific-section')
-
-{{-- ESTADISTICAS --}} {{-- CONVERTIRLO EN UN COMPONENTE LIVEWIRE APARTE --}}
-<div class="m-4">
-    <div class="row">
-        <div class="col">
-            <div class="card h-100 mb-4">
-                <div class="card-header pb-0 px-3 pt-2">
-                    <h6 class="text-uppercase text-body text-md font-weight-bolder"><i class="far fa-chart-bar"></i>
-                        Estadisticas</h6>
-                </div>
-                <div class="card-body pt-2 p-3">
-                    {{-- @include('livewire\management\metrics\layout\metric-list') --}}
+    {{-- ESTADISTICAS --}} {{-- CONVERTIRLO EN UN COMPONENTE LIVEWIRE APARTE --}}
+    <div class="m-4">
+        <div class="row">
+            <div class="col">
+                <div class="card h-100 mb-4">
+                    <div class="card-header pb-0 px-3 pt-2">
+                        <h6 class="text-uppercase text-body text-md font-weight-bolder"><i class="far fa-chart-bar"></i>
+                            Estadisticas</h6>
+                    </div>
+                    <div class="card-body pt-2 p-3">
+                        {{-- @include('livewire\management\metrics\layout\metric-list') --}}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
-{{-- HISTORICO --}} {{-- CONVERTIRLO EN UN COMPONENTE LIVEWIRE APARTE --}}
-<div class="m-4">
-    <div class="row">
-        <div class="col">
-            <div class="card h-100 mb-4">
-                <div class="card-header pb-0 px-3 pt-2">
-                    <h6 class="text-uppercase text-body text-md font-weight-bolder"><i class="fas fa-calendar-alt"></i>
-                        Historico</h6>
-                </div>
-                <div class="card-body pt-2 p-3">
-                    {{-- @include('livewire\management\metrics\layout\metric-list') --}}
+    {{-- HISTORICO --}} {{-- CONVERTIRLO EN UN COMPONENTE LIVEWIRE APARTE --}}
+    <div class="m-4">
+        <div class="row">
+            <div class="col">
+                <div class="card h-100 mb-4">
+                    <div class="card-header pb-0 px-3 pt-2">
+                        <h6 class="text-uppercase text-body text-md font-weight-bolder"><i
+                                class="fas fa-calendar-alt"></i>
+                            Historico</h6>
+                    </div>
+                    <div class="card-body pt-2 p-3">
+                        {{-- @include('livewire\management\metrics\layout\metric-list') --}}
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/livewire/management/metrics/template/metric-test-performance.blade.php
+++ resources/views/livewire/management/metrics/template/metric-test-performance.blade.php
@@ -8,43 +8,45 @@
 
 
 @section('metric-template-actions')
-@include('livewire.management.metrics.template.metric-test-performance.list-modules')
-@include('livewire.management.metrics.template.metric-test-performance.create-module')
-@include('livewire.management.metrics.template.metric-test-performance.create-field')
-@include('livewire.management.metrics.template.metric-test-performance.edit-metric')
+    @include('livewire.management.metrics.template.metric-test-performance.list-modules')
+    @include('livewire.management.metrics.template.metric-test-performance.create-module')
+    @include('livewire.management.metrics.template.metric-test-performance.create-field')
+    @include('livewire.management.metrics.template.metric-test-performance.edit-metric')
 @endsection
 
 
+
 @section('metric-template-content')
-<div class="m-4">
-    <div class="row">
-        <div class="col">
-            <div class="card h-100 mb-4">
-                <div class="card-header pb-0 px-3 pt-2 d-flex justify-content-between">
-                    <div>
-                        <h6 class="text-uppercase text-body text-md font-weight-bolder pt-1"><i
-                                class="fas fa-ruler"></i>
-                            Evaluaciones</h6>
-                    </div>
-                    <div class="px-2">
-                        <div class="input-group input-group-outline">
-                            <label class="form-label">Buscar...</label>
-                            <input type="text" class="form-control" onfocus="focused(this)" onfocusout="defocused(this)"
-                                spellcheck="false" data-ms-editor="true">
+    <div class="m-4">
+        <div class="row">
+            <div class="col">
+                <div class="card h-100 mb-4">
+                    <div class="card-header pb-0 px-3 pt-2 d-flex justify-content-between">
+                        <div>
+                            <h6 class="text-uppercase text-body text-md font-weight-bolder pt-1"><i
+                                    class="fas fa-ruler"></i>
+                                Evaluaciones</h6>
                         </div>
-                        {{-- <span class="badge badge-sm bg-gradient-primary pt-3 cursor-pointer">FILTROS</span> --}}
+                        <div class="px-2">
+                            <div class="input-group input-group-outline">
+                                <label class="form-label">Buscar...</label>
+                                <input type="text" class="form-control" onfocus="focused(this)" onfocusout="defocused(this)"
+                                    spellcheck="false" data-ms-editor="true">
+                            </div>
+                            {{-- <span class="badge badge-sm bg-gradient-primary pt-3 cursor-pointer">FILTROS</span> --}}
+                        </div>
                     </div>
-                </div>
-                <div class="card-body pt-0 p-3">
-                    @if ($metrics_with_out_fields->count() == 1)
-                    <sub class="mt-3"> Necesita asignar/crear nuevos test para el modulo: </sub>
-                    @elseif ($metrics_with_out_fields->count() > 1)
-                    <sub class="mt-3"> Necesita asignar/crear nuevos test para los modulos : </sub>
-                    @endif
-                    @include('livewire\management\metrics\template\metric-test-performance.fields')
+                    <div class="card-body pt-0 p-3">
+                        @if ($metrics_with_out_fields->count() == 1)
+                        <sub class="mt-3"> Necesita asignar/crear nuevos test para el modulo: </sub>
+                        @elseif ($metrics_with_out_fields->count() > 1)
+                        <sub class="mt-3"> Necesita asignar/crear nuevos test para los modulos : </sub>
+                        @endif
+
+                        @include('livewire\management\metrics\template\metric-test-performance.fields')
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
